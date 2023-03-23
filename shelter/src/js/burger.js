ec5a6ab93abd51b6867559f76e65a0cf517024e3
ADyBaH <Brandon4@mail.ru>
2023-03-23 15:26:22 +0300

    feat: add complet burger

--- shelter/src/js/burger.js
+++ shelter/src/js/burger.js
@@ -1,11 +1,13 @@
+import { deactivateScroll } from "./modal";
 const burger = document.querySelector(".header__nav_burger");
 const bg = document.querySelector(".bg-popup");
 const navBar = document.querySelector(".header__nav");
-const headerLogo = document.querySelector(".header__logo_h1")
+// const headerLogo = document.querySelector(".header__logo_h1")
 
 function activeBackgroundForPopUp() {
   bg.classList.add("bg-popup_opacity");
   bg.classList.add("bg-popup_visible");
+  deactivateScroll();
 }
 export function deActiveBackgroundForPopUp() {
   bg.classList.remove("bg-popup_opacity");
@@ -14,23 +16,38 @@ export function deActiveBackgroundForPopUp() {
   }, 300);
 }
 
-export function activateBurger() {
-  deActiveBackgroundForPopUp();
-  burger.classList.remove("burger_active");
-  navBar.classList.remove("burger__nav_active");
-  // headerLogo.classList.remove("header_active");
-  navBar.parentNode.classList.remove("header_active");
-}
-
-function deactivateBurger() {
+function activateBurger() {
   activeBackgroundForPopUp();
   burger.classList.add("burger_active");
   navBar.classList.add("burger__nav_active");
   navBar.parentNode.classList.add("header_active");
   // headerLogo.classList.add("header_active");
+  
+}
+
+export function deactivateBurger() {
+  deActiveBackgroundForPopUp();
+  burger.classList.remove("burger_active");
+  navBar.classList.remove("burger__nav_active");
+  // headerLogo.classList.remove("header_active");
+  navBar.parentNode.classList.remove("header_active");
+  deactivateScroll();
 }
 function burgerToggle() {
   burger.classList.contains("burger_active")?
-  activateBurger() : deactivateBurger();
+   deactivateBurger() : activateBurger();
 }
 burger.addEventListener("click", burgerToggle);
+
+const navigation = document.querySelector(".nav__list");
+function closeBurgerAfterClick(event) {
+  const arr = [
+    "About the shelter",
+    "Our pets",
+    "Help the shelter",
+    "Contacts",
+  ]
+  if(!arr.includes(event.target.innerHTML)) return;
+  deactivateBurger();
+}
+navigation.addEventListener("click", closeBurgerAfterClick) 
--- shelter/src/js/modal.js
+++ shelter/src/js/modal.js
@@ -1,15 +1,18 @@
-import { activateBurger } from "./burger.js";
+import { deactivateBurger } from "./burger.js";
 
+export function deactivateScroll() {
+  document.body.classList.toggle("noScroll");
+}
 function closeAllModal(event) {
   if(!event.target.classList.contains("bg-popup")) return;
-  activateBurger();
+  deactivateBurger();
 }
 
 document.addEventListener("click", closeAllModal);
 function checkSize() {
   if (window.innerWidth < 768) return;
   if (!document.querySelector(".header").classList.contains("header_active")) return;
-  activateBurger();
+  deactivateBurger();
 }
 
 window.addEventListener("resize", checkSize)

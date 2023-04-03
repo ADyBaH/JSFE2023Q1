ba1076a8911e53eb78b5a57714fa34da994c4788
ADyBaH <Brandon4@mail.ru>
2023-04-03 20:00:31 +0300

    feat: change logic for changeCard

--- shelter/src/pets/js/changeCards.js
+++ shelter/src/pets/js/changeCards.js
@@ -1,18 +1,37 @@
 export function changeCard(nodeArray, chunk, animation = true) {
-  // console.log(nodeArray)
-  [...nodeArray].forEach((element, index) => {
-    const { img, name } = chunk[index];
+  // console.log(chunk.forEach(element => {
+  //   console.log(element)
+  // }))
+  chunk.forEach((element, index) => {
+    const { img, name } = element;
     if(animation) {
-      element.classList.add("pets__figure_opacity0");
+      nodeArray[index].classList.add("pets__figure_opacity0");
       setTimeout(() => {
-        element.querySelector(".pets__figure_img").src = img;
-        element.querySelector(".pets__figure_title").textContent = name;
-        element.classList.remove("pets__figure_opacity0")
+        nodeArray[index].querySelector(".pets__figure_img").src = img;
+        nodeArray[index].querySelector(".pets__figure_title").textContent = name;
+        nodeArray[index].classList.remove("pets__figure_opacity0")
       }, 400)
     } else {
-      element.querySelector(".pets__figure_img").src = img;
-      element.querySelector(".pets__figure_title").textContent = name;
+      nodeArray[index].querySelector(".pets__figure_img").src = img;
+      nodeArray[index].querySelector(".pets__figure_title").textContent = name;
     }
   });
-
 }
+
+// export function changeCard(nodeArray, chunk, animation = true) {
+//   // console.log(nodeArray)
+//   [...nodeArray].forEach((element, index) => {
+//     const { img, name } = chunk[index];
+//     if(animation) {
+//       element.classList.add("pets__figure_opacity0");
+//       setTimeout(() => {
+//         element.querySelector(".pets__figure_img").src = img;
+//         element.querySelector(".pets__figure_title").textContent = name;
+//         element.classList.remove("pets__figure_opacity0")
+//       }, 400)
+//     } else {
+//       element.querySelector(".pets__figure_img").src = img;
+//       element.querySelector(".pets__figure_title").textContent = name;
+//     }
+//   });
+// }

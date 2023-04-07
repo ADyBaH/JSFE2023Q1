bd6df1cbceb7b0e955fbd525da7525a4dbc3e149
ADyBaH <Brandon4@mail.ru>
2023-04-07 16:05:42 +0300

    fix: fix path for all project

--- shelter/src/js/jsonPets.js
+++ shelter/src/js/jsonPets.js
@@ -1,5 +1,8 @@
+import dataJson from "@/assets/pets.json"
+
 export async function getJsonPets() {
-  const fetchPets = await fetch("../assets/pets.json");
-  const cashJson = await fetchPets.json();
-  return cashJson;
+  // const fetchPets = await fetch("../assets/pets.json");
+  // const cashJson = await fetchPets.json();
+
+  return dataJson;
 }

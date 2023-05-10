744add6efb8a451e6f99b095674dfee1a94d1545
ADyBaH <Brandon4@mail.ru>
2023-05-10 21:47:56 +0300

    refactor: BaseComponent and header

--- minesweeper/src/app/components/base-component.js
+++ minesweeper/src/app/components/base-component.js
@@ -1,8 +1,11 @@
 export class BaseComponent {
   constructor(props) {
-    const { tag } = props;
+    const { tag, attr, parent } = props;
     const element = document.createElement(tag || 'div');
-    Object.assign(element, props);
+    Object.assign(element, attr);
     this.element = element;
+    if(parent) {
+      parent.append(element)
+    }
   }
 }
--- minesweeper/src/app/components/header.js
+++ minesweeper/src/app/components/header.js
@@ -2,13 +2,12 @@ import { BaseComponent } from './base-component';
 
 export class Header extends BaseComponent {
   constructor(root) {
-    super({ tag: 'header', className: 'header' });
-    this.wrapper = new BaseComponent({ tag: 'div', className: 'wrapper' });
-    this.logo = new BaseComponent({ tag: 'h1', textContent: 'RSS-Minesweeper', className: 'header__logo' });
-    this.wrapper.element.append(this.logo.element);
-    this.element.append(this.wrapper.element);
-    if (root) {
-      root.append(this.element);
-    }
+    super({ tag: 'header', attr: { className: 'header' }, parent: root });
+    this.wrapper = new BaseComponent({ attr: { className: 'wrapper' }, parent: this.element });
+    this.logo = new BaseComponent({
+      tag: 'h1',
+      attr: { textContent: 'RSS-Minesweeper', className: 'header__logo' },
+      parent: this.wrapper.element,
+    });
   }
 }

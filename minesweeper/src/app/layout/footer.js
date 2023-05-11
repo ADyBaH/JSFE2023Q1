3ce0e8c5af39ffa82f9ae2f5e44acfd6519ffb72
ADyBaH <Brandon4@mail.ru>
2023-05-11 23:39:57 +0300

    refactor: rewrite footer to innerHTML

--- minesweeper/src/app/layout/footer.js
+++ minesweeper/src/app/layout/footer.js
@@ -1,28 +1,14 @@
 import { BaseComponent } from '../utils/base-component'
-// убрать this
+
 export class Footer extends BaseComponent {
   constructor(root) {
     super({ tag: 'footer', attr: { className: 'footer' }, parent: root })
-    // переписать на иннер
-    this.wrapper = new BaseComponent({ attr: { className: 'wrapper' }, parent: this.element })
-    this.container = new BaseComponent({ attr: { className: 'container' }, parent: this.wrapper.element })
-    this.github = new BaseComponent({
-      tag: 'a',
-      attr: {
-        className: 'footer__github',
-        textContent: 'github',
-        target: '_blank',
-        href: 'https://github.com/ADyBaH',
-      },
-    })
-    this.by = new BaseComponent({
-      tag: 'h2',
-      attr: { className: 'footer__logo', textContent: '© ADyBaH 2023' },
-    })
-    this.rssLogo = new BaseComponent({
-      tag: 'a',
-      attr: { className: 'footer__rss-logo', href: 'https://rs.school/js/' },
-    })
-    this.container.element.append(this.github.element, this.by.element, this.rssLogo.element)
+    this.element.innerHTML = `
+      <div class= "container">
+        <a class = "footer__github" target= "_blank" href="https://github.com/ADyBaH">github</a>
+        <h2 class = "footer__logo">© ADyBaH 2023</h2>
+        <a class = "footer__rss-logo" target= "_blank" href="https://rs.school/js/"></a>
+      </div>
+    `
   }
 }

aa12b1ceeeada18d408889e00fdfa6f3b02c8ed4
ADyBaH <Brandon4@mail.ru>
2023-05-12 00:18:21 +0300

    refactor: change set element to innerHTML

--- minesweeper/src/app/components/header.js
+++ minesweeper/src/app/components/header.js
@@ -3,12 +3,6 @@ import { BaseComponent } from '../utils/base-component'
 export class Header extends BaseComponent {
   constructor(root) {
     super({ tag: 'header', attr: { className: 'header' }, parent: root })
-    // переписать на inner html
-    this.wrapper = new BaseComponent({ attr: { className: 'wrapper' }, parent: this.element })
-    this.logo = new BaseComponent({
-      tag: 'h1',
-      attr: { textContent: 'RSS-Minesweeper', className: 'header__logo' },
-      parent: this.wrapper.element,
-    })
+    this.element.innerHTML = '<h1 class = "header__logo">RSS-Minesweeper</h1>'
   }
 }

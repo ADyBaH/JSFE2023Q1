f64a7bc06493cdb7af10ec07b6024cbf62e72e5f
ADyBaH <Brandon4@mail.ru>
2023-05-21 20:25:12 +0300

    feat: add utils set-lose

---
+++ minesweeper/src/utils/set-lose.js
@@ -0,0 +1,14 @@
+import { setToLocalStorage } from './set-to-localstorage'
+
+export function setLose(state, header, modal, options, target, soundLose) {
+  Object.assign(target, { textContent: '💥' })
+  Object.assign(options.element, { className: 'game__options game__options_hide' })
+  Object.assign(modal.text.element, { textContent: 'Game over. Try again', className: 'game__modal_logo logo_lose' })
+  Object.assign(modal.element, { className: 'game__modal' })
+  Object.assign(header.buttonNewGame.element, { className: 'button-new-game button-new-game-lose' })
+  setToLocalStorage(`Lose: difficult: ${state.difficult}, count of mine: ${state.countOfMine}, seconds: ${state.timer}`)
+  if (state.isSound) {
+    soundLose.play()
+  }
+  header.timer.stopTimer()
+}

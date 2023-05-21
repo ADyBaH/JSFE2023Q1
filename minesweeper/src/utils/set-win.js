5c65b3cb526daee6c0699f365f2e9ffc51ea3407
ADyBaH <Brandon4@mail.ru>
2023-05-21 20:19:25 +0300

    feat: add utils set-win

---
+++ minesweeper/src/utils/set-win.js
@@ -0,0 +1,16 @@
+import { setToLocalStorage } from './set-to-localstorage'
+
+export function setWin(state, header, modal, options, soundWin) {
+  Object.assign(options.element, { className: 'game__options game__options_hide' })
+  Object.assign(modal.text.element, {
+    textContent: `Hooray! You found all mines in ${state.timer} seconds and ${state.countOfMove} moves!`,
+    className: 'game__modal_logo logo_win',
+  })
+  Object.assign(modal.element, { className: 'game__modal' })
+  Object.assign(header.buttonNewGame.element, { className: 'button-new-game button-new-game-win' })
+  setToLocalStorage(`Win: difficult: ${state.difficult}, count of mine: ${state.countOfMine}, seconds: ${state.timer}`)
+  if (state.isSound) {
+    soundWin.play()
+  }
+  header.timer.stopTimer()
+}

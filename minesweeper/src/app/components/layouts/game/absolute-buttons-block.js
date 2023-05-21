97ad1d11bd5329e981cdb151e54192522a181fb8
ADyBaH <Brandon4@mail.ru>
2023-05-21 21:15:05 +0300

    feat: add class absolute-buttons-block

---
+++ minesweeper/src/app/components/layouts/game/absolute-buttons-block.js
@@ -0,0 +1,67 @@
+import { BaseComponent } from '../../../../utils/base-component'
+import { setTextToModal } from '../../../../utils/set-text-to-modal'
+
+export class AbsoluteButtonsBlock extends BaseComponent {
+  constructor(parent, state, modal) {
+    super({ attr: { className: 'absolute-button-block' }, parent })
+    this.state = state
+    this.modal = modal
+    this.theme = new BaseComponent({
+      tag: 'button',
+      attr: { className: 'absolute-button-block__theme', textContent: '🌒' },
+      parent: this.element,
+      events: [
+        {
+          name: 'click',
+          callback: () => this.toggleTheme(),
+        },
+      ],
+    })
+    this.sound = new BaseComponent({
+      tag: 'button',
+      attr: { className: 'absolute-button-block__sound', textContent: '🔈' },
+      parent: this.element,
+      events: [
+        {
+          name: 'click',
+          callback: () => this.toggleSound(),
+        },
+      ],
+    })
+    this.score = new BaseComponent({
+      tag: 'button',
+      attr: { className: 'absolute-button-block__score', textContent: '🔝' },
+      parent: this.element,
+      events: [
+        {
+          name: 'click',
+          callback: () => this.toggleScore(),
+        },
+      ],
+    })
+  }
+
+  toggleTheme() {
+    this.theme.element.textContent = this.theme.element.textContent === '☀️' ? '🌒' : '☀️'
+    this.state.theme = this.theme.element.textContent === '☀️' ? 'white' : 'dark'
+    document.body.className = this.state.theme
+  }
+
+  toggleSound() {
+    this.sound.element.textContent = this.sound.element.textContent === '🔈' ? '🔇' : '🔈'
+    this.state.isSound = !this.state.isSound
+  }
+
+  toggleScore() {
+    const checkActive = (isActive) =>
+      isActive
+        ? this.modal.element.classList.add('game__modal_hide')
+        : this.modal.element.classList.remove('game__modal_hide')
+
+    checkActive(this.score.element.classList.contains('active'))
+
+    Object.assign(this.modal.text.element, { className: 'game__modal_logo' })
+    this.score.element.classList.toggle('active')
+    setTextToModal(this.modal.text.element, JSON.parse(localStorage.getItem('results')).join('\n\n'))
+  }
+}

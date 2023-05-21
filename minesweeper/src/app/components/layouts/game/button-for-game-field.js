f6b6058568591ed69e1d7b57863780877895cecb
ADyBaH <Brandon4@mail.ru>
2023-05-21 21:14:16 +0300

    feat: add class button-for-game-field

---
+++ minesweeper/src/app/components/layouts/game/button-for-game-field.js
@@ -0,0 +1,29 @@
+import { BaseComponent } from '../../../../utils/base-component'
+
+export class ButtonForPlayground extends BaseComponent {
+  constructor({ field, isMine = false, isOpen = false, isChecked = false, numberOfMineAround = 0, events }, index) {
+    super({
+      tag: 'button',
+      attr: { className: 'button', textContent: '' },
+      parent: field,
+      events,
+    })
+    this.matrixIndex = index
+    this.element.dataset.matrixIndex = index
+    // переписать на объект и брать гетером
+    this.isMine = isMine
+    this.isChecked = isChecked
+    this.isOpen = isOpen
+
+    this.numberOfMineAround = numberOfMineAround
+  }
+
+  setStateButton({ state, text }) {
+    if (state) {
+      Object.assign(this, state)
+    }
+    if (text) {
+      Object.assign(this.element, text)
+    }
+  }
+}

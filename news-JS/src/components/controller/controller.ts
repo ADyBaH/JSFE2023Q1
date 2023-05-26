725a3445f66486dd89c9d99fc4a8e2d9b2642f12
ADyBaH <Brandon4@mail.ru>
2023-05-26 16:57:35 +0300

    feat: add controller.ts

---
+++ news-JS/src/components/controller/controller.ts
@@ -0,0 +1,42 @@
+import AppLoader from './appLoader'
+import { DrawSources, DrawNews } from '../types/appTypes'
+import { EnumController } from '../types/enumAll'
+
+class AppController extends AppLoader {
+  getSources(callback: (data: DrawSources) => void): void {
+    super.getResp(
+      {
+        endpoint: EnumController.endpointSources,
+      },
+      callback,
+    )
+  }
+
+  getNews(e: MouseEvent, callback: (data: DrawNews) => void): void {
+    let target = e.target as HTMLDivElement
+    const newsContainer = e.currentTarget as HTMLSpanElement
+
+    while (target !== newsContainer) {
+      if (target.classList.contains(EnumController.sourcItem)) {
+        const sourceId = target.getAttribute(EnumController.dataSourceId) as string
+        if (newsContainer.getAttribute(EnumController.dataSource) !== sourceId) {
+          newsContainer.setAttribute(EnumController.dataSource, sourceId)
+          super.getResp(
+            {
+              endpoint: EnumController.endpointEverything,
+              options: {
+                sources: sourceId,
+              },
+            },
+            callback,
+          )
+        }
+        return
+      }
+
+      target = target.parentNode as HTMLDivElement
+    }
+  }
+}
+
+export default AppController

65b1c29bd82502522b65cac61ec67d7e27060d6b
ADyBaH <Brandon4@mail.ru>
2023-05-26 16:12:47 +0300

    feat: add webpack setup

---
+++ news-JS/webpack.dev.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    // contentBase: path.resolve(__dirname, '../dist'),
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+}

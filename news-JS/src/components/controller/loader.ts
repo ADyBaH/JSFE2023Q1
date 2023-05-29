bc0891db5d4296a7c63bf9191c308697b1a77262
ADyBaH <Brandon4@mail.ru>
2023-05-29 21:39:47 +0300

    refactor: remove useless fetch and makeUrl

--- news-JS/src/components/controller/loader.ts
+++ news-JS/src/components/controller/loader.ts
@@ -1,60 +1,42 @@
-import { ResponseCodes } from '../model/response-codes.enum'
-import { DrawNews, DrawSources, Options, Resp } from '../types/appTypes'
+import { ResponseMethods } from '../model/response-methods.enum'
+import { DataOrPost, Options, Resp } from '../types/custom-type'
 
 class Loader {
   protected readonly baseLink: string
   protected readonly options: Options
-  // constructor(baseLink, options) {
+
   constructor(baseLink: string, options: Options) {
     this.baseLink = baseLink
     this.options = options
   }
 
-  private getIsResponseError(response: Response): boolean {
-    return response.status === ResponseCodes.Unauthorized || response.status === ResponseCodes.NotFound
-  }
-
-  protected getResp<Type extends DrawNews | DrawSources>(
-    { endpoint, options }: Resp,
-    callback: (data: Type) => void = (): void => {
-      console.error('No callback for GET response')
-      // throw new Error('No callback for GET response');
-    },
-  ): void {
-    this.load('GET', endpoint, callback, options)
-  }
-
-  protected errorHandler(response: Response): Response {
-    if (this.getIsResponseError(response)) {
-      throw Error(response.statusText)
+  protected async getResp<Type extends DataOrPost>(
+    { endpoint, sourceId }: Resp,
+    callback: (data: Type) => void,
+  ): Promise<void> {
+    try {
+      await this.load(ResponseMethods.GET, endpoint, callback, sourceId)
+    } catch (error) {
+      console.error(error)
     }
-    return response
   }
 
-  // makeUrl(options, endpoint) {
-  protected makeUrl(options: Options | undefined, endpoint: string): string {
-    const urlOptions: Options = { ...this.options, ...options }
-    let url = `${this.baseLink}${endpoint}?`
-
-    ;(Object.keys(urlOptions) as (keyof Options)[]).forEach((key) => {
-      url += `${key}=${urlOptions[key]}&`
-    })
-    return url.slice(0, -1)
-  }
-
-  // load(method, endpoint, callback, options = {}) {
-  // load(method: string, endpoint: string, callback: (data: string) => void, options: Options) {
-  protected load<T extends DrawNews | DrawSources>(
+  protected async load<T extends DataOrPost>(
     method: string,
     endpoint: string,
     callback: (data: T) => void,
-    options?: Options,
-  ): void {
-    fetch(this.makeUrl(options, endpoint), { method })
-      .then(this.errorHandler)
-      .then((res) => res.json())
-      .then((data) => callback(data))
-      .catch((err) => console.error(err))
+    sourceId: string = '',
+  ): Promise<void> {
+    const response = await fetch(`${this.baseLink}${endpoint}?apiKey=${this.options.apiKey}&sources=${sourceId}`, {
+      method,
+    })
+
+    if (!response.ok) {
+      throw Error(response.statusText)
+    }
+
+    const data: T = await response.json()
+    callback(data)
   }
 }
 

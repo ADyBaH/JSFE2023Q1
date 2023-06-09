de4606e2cbf684d06bd717aa1f2ef3968e84b3ac
ADyBaH <Brandon4@mail.ru>
2023-06-09 22:04:12 +0300

    feat: add base template for main

--- rs-css/src/app/components/layouts/main.ts
+++ rs-css/src/app/components/layouts/main.ts
@@ -1,6 +1,13 @@
 import { BaseComponent } from '../../../utils/base-component'
+import { Editor } from './main/editor/editor'
+import { Levels } from './main/levels'
+import { TableBlock } from './main/table-block'
 
 export class Main extends BaseComponent {
+  public table = new TableBlock(this.element)
+  public editor = new Editor(this.element)
+  public levels = new Levels(this.element)
+
   constructor(root: HTMLElement) {
     super({ tag: 'main', attribute: { className: 'main' }, parent: root })
   }

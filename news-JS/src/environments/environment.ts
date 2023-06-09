88f02fd6608ae9c56784b1cf8a7193d3b728d9ec
ADyBaH <Brandon4@mail.ru>
2023-06-09 22:55:20 +0300

    feat: add school proxy

--- news-JS/src/environments/environment.ts
+++ news-JS/src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { Environment } from '../components/types/custom-type'
 
 export const environment: Readonly<Environment> = {
-  BASE_URL: 'https://newsapi.org/v2/',
+  BASE_URL: 'https://rss-news-api.onrender.com/',
   API_KEY: '765a68b0bc3f472a847589faf643027c',
 }

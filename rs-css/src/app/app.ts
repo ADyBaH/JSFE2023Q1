fb6bbd4e9c5e232bf9547d379945c93e42cc669c
ADyBaH <Brandon4@mail.ru>
2023-06-11 16:24:31 +0300

    refactor: for app

--- rs-css/src/app/app.ts
+++ rs-css/src/app/app.ts
@@ -1,14 +1,15 @@
 import { Layout } from './components/layouts/layout'
 
 class App {
-  public init(): Layout | boolean {
-    const root = document.getElementById('root')
-    if (root instanceof HTMLElement) {
-      const layout = new Layout()
-      layout.create(root)
-      return layout
+  public layout = new Layout()
+  private root = document.getElementById('root')
+
+  public init(): boolean {
+    if (!this.root) {
+      return false
     }
-    return false
+    this.layout.create(this.root)
+    return true
   }
 }
 

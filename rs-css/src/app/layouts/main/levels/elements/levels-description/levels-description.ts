c52895f1b9784bbb9a052630b7fa31d5a8438f8f
ADyBaH <Brandon4@mail.ru>
2023-06-23 23:51:02 +0300

    feat: add levels-description element

---
+++ rs-css/src/app/layouts/main/levels/elements/levels-description/levels-description.ts
@@ -0,0 +1,31 @@
+import './levels-description.scss'
+import { BaseComponent } from '../../../../../../utils/base-component'
+import { LevelInterface } from '../../../../../modules/interface-for-levels'
+import { emitter } from '../../../../../services/event-emitter'
+
+export class LevelsDescription extends BaseComponent {
+  public buttonToggleClose = new BaseComponent({
+    tag: 'button',
+    attribute: { className: 'levels-block__close-open-button levels-block__close-open-button_rotate' },
+    parent: this.element,
+  })
+
+  private descriptionBlock = new BaseComponent({
+    attribute: { className: 'levels-block__description-block' },
+    parent: this.element,
+  })
+  constructor(parent: HTMLElement) {
+    super({ attribute: { className: 'levels-block__description levels-block__description_hidden' }, parent })
+    this.buttonToggleClose.setEventListener('click', () => this.toggleCloseDescription())
+    emitter.subscribe('changeLevel', (args: LevelInterface) => this.changeDescription(args))
+  }
+
+  private toggleCloseDescription(): void {
+    this.buttonToggleClose.toggle('levels-block__close-open-button_rotate')
+    this.toggle('levels-block__description_hidden')
+  }
+
+  public changeDescription(level: LevelInterface): void {
+    this.descriptionBlock.innerHTML = level.description
+  }
+}

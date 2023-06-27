a86da0459f36c9b944a6f115e29600e059d45eab
ADyBaH <Brandon4@mail.ru>
2023-06-27 23:17:03 +0300

    feat: change editor

--- rs-css/src/app/layouts/main/editor/editor.ts
+++ rs-css/src/app/layouts/main/editor/editor.ts
@@ -1,16 +1,17 @@
-import { BaseComponent } from '../../../components/base-component'
-import { CssEditor } from './css-editor'
-import { FileBlock } from './file-block'
-import { HtmlViewer } from './html-viewer'
+import { BaseComponent } from '../../../../utils/base-component'
+import { HtmlViewer } from './elements/html-viewer/html-viewer'
+import { CssEditor } from './elements/css-editor/css-editor'
+import { FileBlock } from './elements/file-block/file-block'
+import './editor.scss'
 
 export class Editor extends BaseComponent {
   public cssEditor: CssEditor
   public htmlViewer: HtmlViewer
-  constructor(root: HTMLElement) {
+  constructor(root: HTMLElement, tableElement: HTMLElement) {
     super({ attribute: { className: 'editor-block' }, parent: root })
     const cssBlock = new FileBlock(this.element, 'css')
     const htmlBlock = new FileBlock(this.element, 'html')
-    this.cssEditor = new CssEditor(cssBlock.fileBlock.element)
+    this.cssEditor = new CssEditor(cssBlock.fileBlock.element, tableElement)
     this.htmlViewer = new HtmlViewer(htmlBlock.fileBlock.element)
   }
 }

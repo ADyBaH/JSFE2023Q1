54fa5f1ab2161b3258350dcde72884a4a841bfb8
ADyBaH <Brandon4@mail.ru>
2023-06-27 22:55:54 +0300

    feat: add stringTemplateForInputBlock

---
+++ rs-css/src/app/constants/css-editor-constant.ts
@@ -0,0 +1,11 @@
+export const stringTemplateForInputBlock: string = `
+<br>
+<p class="css-editor__text"></p>
+<p class="css-editor__text">{</p>
+<p class="css-editor__text">/* Styles would go here. */</p>
+<p class="css-editor__text">}</p>
+<br>
+<p class="css-editor__text">/*</p>
+<p class="css-editor__text">Type a number to skip to a level.</p>
+<p class="css-editor__text">Ex → "5" for level 5</p><p class="css-editor__text">*/</p>
+`

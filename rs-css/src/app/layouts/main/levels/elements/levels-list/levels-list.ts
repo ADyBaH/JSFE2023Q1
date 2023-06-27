9101dd4d8f89b15ce174d72d9c2f2a56cd4c2565
ADyBaH <Brandon4@mail.ru>
2023-06-27 23:19:31 +0300

    feat: add method setup win for levels-list

--- rs-css/src/app/layouts/main/levels/elements/levels-list/levels-list.ts
+++ rs-css/src/app/layouts/main/levels/elements/levels-list/levels-list.ts
@@ -1,5 +1,6 @@
-import { LevelsDataInterface } from '../../../../../modules/interface-for-levels'
+import { LevelsDataInterface } from '../../../../../models/interface-for-levels'
 import { levelsData } from '../../../../../../assets/data/levels-data.json'
+import { localStorageADyBaH } from '../../../../../services/local-storage'
 import { BaseComponent } from '../../../../../../utils/base-component'
 import { emitter } from '../../../../../services/event-emitter'
 import './levels-list.scss'
@@ -20,6 +21,7 @@ export class LevelsList extends BaseComponent {
       attribute: { className: 'levels-block__reset-button', textContent: 'Reset levels.' },
       parent: this.element,
     })
+    emitter.subscribe('setupWin', () => this.setupWin())
     this.arrayButtons.forEach((button) => button.setEventListener('click', (event: Event) => this.eventClick(event)))
     this.resetButton.setEventListener('click', () => this.resetLevels())
   }
@@ -55,5 +57,15 @@ export class LevelsList extends BaseComponent {
   public resetLevels(): void {
     emitter.emit('resetLevels')
     this.arrayButtons.forEach((baseComponent) => baseComponent.removeClass('levels-block__button_completed'))
+  }
+
+  private setupWin(): void {
+    this.completedTask = localStorageADyBaH.completedTask
+    this.arrayButtons.forEach((baseComponent) => {
+      const value = baseComponent.element.textContent?.split(' ')[0]
+      if (value && this.completedTask.includes(value)) {
+        baseComponent.addClass('levels-block__button_completed')
+      }
+    })
   }
 }

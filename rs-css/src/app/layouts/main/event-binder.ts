58b8fb79b8e32776d451970cc5671d7b4585b4e4
ADyBaH <Brandon4@mail.ru>
2023-06-29 14:51:01 +0300

    feat: add title for hover event

--- rs-css/src/app/layouts/main/event-binder.ts
+++ rs-css/src/app/layouts/main/event-binder.ts
@@ -3,10 +3,16 @@ import { BaseComponent } from '../../../utils/base-component'
 export class EventBinder {
   public tableElement: BaseComponent
   public editorElement: BaseComponent
+  public title: BaseComponent
   constructor(tableElement: BaseComponent, editorElement: BaseComponent) {
     this.tableElement = tableElement
     this.editorElement = editorElement
-
+    this.title = new BaseComponent({
+      attribute: {
+        className: 'title',
+        textContent: `<${tableElement.element.tagName}></${tableElement.element.tagName}>`,
+      },
+    })
     this.editorElement.setEventListener('mouseover', (event) => this.toggleHoverClassName(event))
     this.tableElement.setEventListener('mouseover', (event) => this.toggleHoverClassName(event))
     this.editorElement.setEventListener('mouseout', (event) => this.toggleHoverClassName(event))
@@ -17,5 +23,19 @@ export class EventBinder {
     event.stopPropagation()
     this.editorElement.toggle('active-text')
     this.tableElement.toggle('active')
+    if (event.type === 'mouseover') {
+      this.addTitle()
+    }
+    if (event.type === 'mouseout') {
+      this.removeTitle()
+    }
+  }
+
+  public addTitle(): void {
+    this.tableElement.element.append(this.title.element)
+  }
+
+  public removeTitle(): void {
+    this.title.element.remove()
   }
 }

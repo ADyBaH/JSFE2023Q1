6f626276f97cff647533627fe6828c66331430ac
ADyBaH <Brandon4@mail.ru>
2023-07-02 17:02:57 +0300

    refactor: remove magick string for levels

---
+++ rs-css/src/app/constants/levels-description-constant.ts
@@ -0,0 +1,1 @@
+export const levelsDiscriptionInnerHTMLString = '<h3 class="levels-block__description-text">Description:</h3>'
--- rs-css/src/app/layouts/main/levels/levels.ts
+++ rs-css/src/app/layouts/main/levels/levels.ts
@@ -1,16 +1,22 @@
+import { levelsDiscriptionInnerHTMLString } from '../../../constants/levels-description-constant'
 import { LevelsDescription } from './elements/levels-description/levels-description'
 import { levelsData } from '../../../../assets/data/levels-data.json'
 import { LevelsHeader } from './elements/levels-header/levels-header'
 import { BaseComponent } from '../../../../utils/base-component'
 import { LevelsList } from './elements/levels-list/levels-list'
 import './levels.scss'
+import { MaxMinLevelEnum } from '../../../enum/max-min-level-enum'
 
 export class Levels extends BaseComponent {
   public levelsData = levelsData
   private levelList = new LevelsList(this.element)
   public progressBar = new BaseComponent({
     tag: 'progress',
-    attribute: { className: 'levels-block__progress-bar', value: '1', max: '10' },
+    attribute: {
+      className: 'levels-block__progress-bar',
+      value: `${MaxMinLevelEnum.min}`,
+      max: `${MaxMinLevelEnum.max}`,
+    },
   })
   private levelsHeader: LevelsHeader
   private description: LevelsDescription
@@ -23,9 +29,6 @@ export class Levels extends BaseComponent {
 
     this.description = new LevelsDescription(this.element)
 
-    this.progressBar.element.insertAdjacentHTML(
-      'afterend',
-      '<h3 class="levels-block__description-text">Description:</h3>',
-    )
+    this.progressBar.element.insertAdjacentHTML('afterend', levelsDiscriptionInnerHTMLString)
   }
 }

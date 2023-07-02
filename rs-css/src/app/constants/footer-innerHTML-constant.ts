9bb860df62af955ea256ce2628c10e77eb59fbc4
ADyBaH <Brandon4@mail.ru>
2023-07-02 16:53:15 +0300

    refactor: remove magick string for footer

---
+++ rs-css/src/app/constants/footer-innerHTML-constant.ts
@@ -0,0 +1,7 @@
+export const footerInnerHTMLString = `
+<div class= "container">
+  <a class = "footer__github" target= "_blank" href="https://github.com/ADyBaH">github</a>
+  <h2 class = "footer__logo">© ADyBaH4uK 2023</h2>
+  <a class = "footer__rss-logo" target= "_blank" href="https://rs.school/js/"></a>
+</div>
+`

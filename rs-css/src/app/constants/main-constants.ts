9cd2f3cf0a4b40fdf884f89e8d9284ae8ac59dfc
ADyBaH <Brandon4@mail.ru>
2023-07-03 20:13:01 +0300

    refactor: add destructuring, add function changeMainState, remove magick number

---
+++ rs-css/src/app/constants/main-constants.ts
@@ -0,0 +1,1 @@
+export const endShakeAnimation = 2000
--- rs-css/src/app/layouts/main/main.ts
+++ rs-css/src/app/layouts/main/main.ts
@@ -32,43 +32,50 @@ export class Main extends BaseComponent {
     this.levels = new Levels(this.element)
     this.editor = new Editor(this.element, this.tableBlock.table.element)
 
-    emitter.subscribe(EmitterEnum.changeLevel, (args: LevelInterface) => this.changeState(args))
-    emitter.subscribe(EmitterEnum.shakeEditor, () => this.shakeTable())
-    emitter.emit(EmitterEnum.changeLevel, this.levelsData[this.lastTask])
+    emitter.subscribe(EmitterEnum.ChangeLevel, (args: LevelInterface) => this.changeState(args))
+    emitter.subscribe(EmitterEnum.ShakeEditor, () => this.shakeTable())
+    emitter.emit(EmitterEnum.ChangeLevel, this.levelsData[this.lastTask])
   }
 
+  // переиминовать деструктуризировать
   private changeState({ layout, answer, id, task }: LevelInterface): void {
     this.mainState.editorComponents = []
     this.mainState.tableComponents = []
 
-    layout.forEach((setup) => {
-      const elements = new EventBinder(new TableElement(setup), new EditorElement(setup), setup.innerHTML)
+    layout.forEach(({ tag, attribute, animated, innerHTML, child }) => {
+      const elements = new EventBinder(
+        new TableElement(tag, attribute, animated),
+        new EditorElement(innerHTML),
+        innerHTML,
+      )
 
       this.mainState.editorComponents.push(elements.editorElement)
       this.mainState.tableComponents.push(elements.tableElement)
 
-      if (setup.child) {
+      if (child) {
         const elementsChild = new EventBinder(
-          new TableElement(setup.child, elements.tableElement.element),
-          new EditorElement(setup.child, elements.editorElement.element),
-          setup.child.innerHTML,
+          new TableElement(child.tag, child.attribute, child.animated, elements.tableElement.element),
+          new EditorElement(child.innerHTML, elements.editorElement.element),
+          child.innerHTML,
         )
 
         elementsChild.tableElement.addClass('custom-child-element')
         elementsChild.tableElement.removeClass('custom-element')
         elementsChild.editorElement.element.insertAdjacentHTML(
           'afterend',
-          `<span class="hljs-tag">&lt;/<span class="hljs-name">${setup.tag}</span>&gt;</span>`,
+          `<span class="hljs-tag">&lt;/<span class="hljs-name">${tag}</span>&gt;</span>`,
         )
       }
     })
 
+    this.changeMainState(answer, id, task)
+  }
+  private changeMainState(answer: string, id: string, task: string): void {
     mainState.answer = answer
     mainState.levelId = id
     mainState.mainTask = task
-    emitter.emit(EmitterEnum.changeElementsOnState, mainState)
+    emitter.emit(EmitterEnum.ChangeElementsOnState, mainState)
   }
-
   public shakeTable(): void {
     this.editor.addClass('shake')
     setInterval(() => this.editor.removeClass('shake'), 2000)

86d3729e3f8ed7062c599bd8f9a094bfb62f57c1
ADyBaH <Brandon4@mail.ru>
2023-07-15 22:01:13 +0300

    feat: add garage class and form class

---
+++ async-race/src/app/pages/garage-page/components/form/form.ts
@@ -0,0 +1,77 @@
+import { BaseComponent } from 'src/app/components/base-component'
+
+export class Form extends BaseComponent {
+  private fieldset = new BaseComponent({
+    tag: 'fieldset',
+    parent: this.element,
+  })
+
+  private inputText
+  private inputColor
+  private buttonSubmit
+
+  constructor(className: string, legendName: string, parent: HTMLElement) {
+    super({
+      tag: 'form',
+      attribute: {
+        className,
+      },
+      parent,
+    })
+    this.inputText = new BaseComponent({
+      tag: 'input',
+      attribute: {
+        className: `${className}__input`,
+        type: 'text',
+        placeholder: 'Title',
+      },
+      parent: this.fieldset.element,
+    })
+
+    this.inputColor = new BaseComponent({
+      tag: 'input',
+      attribute: {
+        className: `${className}__color`,
+        type: 'color',
+      },
+      parent: this.fieldset.element,
+    })
+
+    this.buttonSubmit = new BaseComponent({
+      tag: 'button',
+      attribute: {
+        className: `${className}__button`,
+        type: 'submit',
+        textContent: legendName.split(' ')[0],
+      },
+      parent: this.fieldset.element,
+    })
+    this.fieldset.element.insertAdjacentHTML('beforeend', `<legend class="${className}__legend">${legendName}</legend>`)
+  }
+
+  public set color(value: string) {
+    if (this.inputColor.element instanceof HTMLInputElement) {
+      this.inputColor.element.value = `#${value}`
+    }
+  }
+
+  public get color(): string {
+    if (this.inputColor.element instanceof HTMLInputElement) {
+      return this.inputColor.element.value
+    }
+    return ''
+  }
+
+  public set text(value: string) {
+    if (this.inputText.element instanceof HTMLInputElement) {
+      this.inputText.element.value = value
+    }
+  }
+
+  public get text(): string {
+    if (this.inputText.element instanceof HTMLInputElement) {
+      return this.inputText.element.value
+    }
+    return ''
+  }
+}
--- async-race/src/app/pages/garage-page/garage.ts
+++ async-race/src/app/pages/garage-page/garage.ts
@@ -1,7 +1,60 @@
-import { BaseComponent } from 'app/components/base-component'
+import { BaseComponent } from 'src/app/components/base-component'
+import { carSvg } from 'src/app/constants/carSvg'
 import './garage.scss'
+import { Form } from './components/form/form'
 
 export class Garage extends BaseComponent {
+  private inputsContainer = new BaseComponent({
+    attribute: { className: 'inputs-container' },
+    parent: this.element,
+  })
+
+  private createForm = new Form('garage-form', 'Create Car', this.inputsContainer.element)
+  private changeForm = new Form('change-form', 'Change Car', this.inputsContainer.element)
+
+  private buttonCreate = new BaseComponent({
+    tag: 'button',
+    attribute: {
+      className: 'button-race button',
+      textContent: 'Race',
+      type: 'button',
+    },
+    parent: this.inputsContainer.element,
+  })
+
+  private buttonReset = new BaseComponent({
+    tag: 'button',
+    attribute: {
+      className: 'button-reset button',
+      textContent: 'Reset',
+      type: 'button',
+    },
+    parent: this.inputsContainer.element,
+  })
+
+  private buttonGenerateCar = new BaseComponent({
+    tag: 'button',
+    attribute: {
+      className: 'button-generate-car button',
+      textContent: 'Generate Car',
+      type: 'button',
+    },
+    parent: this.inputsContainer.element,
+  })
+
+  private logo = new BaseComponent({
+    tag: 'h1',
+    attribute: {
+      className: 'garage__logo',
+      textContent: 'Garage(0 cars)',
+    },
+    parent: this.element,
+  })
+  private car = new BaseComponent({
+    attribute: { className: 'container-car', innerHTML: carSvg },
+    parent: this.element,
+  })
+
   constructor(parent?: HTMLElement) {
     super({
       attribute: {
@@ -9,6 +62,12 @@ export class Garage extends BaseComponent {
       },
       parent,
     })
-    this.innerHTML = '<h1>Garage</h1>'
+    this.createForm.setEventListener('submit', (event) => this.submitCreateFrom(event))
+    this.changeForm.setEventListener('submit', (event) => this.submitCreateFrom(event))
+  }
+
+  private submitCreateFrom(event: Event): void {
+    event.preventDefault()
+    this.car.element.style.fill = this.createForm.color
   }
 }

55267f032e7c471de8fa0ff95a06fe9e46acd31e
ADyBaH <Brandon4@mail.ru>
2023-07-15 21:56:29 +0300

    feat: add navigation buttons on header

--- async-race/src/app/components/header/header.ts
+++ async-race/src/app/components/header/header.ts
@@ -2,9 +2,45 @@ import { BaseComponent } from '../base-component'
 import './header.scss'
 
 export class Header extends BaseComponent {
+  private navigation = new BaseComponent({
+    tag: 'nav',
+    attribute: {
+      className: 'navigation',
+    },
+    parent: this.element,
+  })
+
+  private navigationButtonGarage = new BaseComponent({
+    tag: 'button',
+    attribute: {
+      className: 'button',
+      type: 'button',
+      textContent: 'Garage',
+    },
+    parent: this.navigation.element,
+  })
+
+  private navigationButtonWinners = new BaseComponent({
+    tag: 'button',
+    attribute: {
+      className: 'button',
+      type: 'button',
+      textContent: 'Winners',
+    },
+    parent: this.navigation.element,
+  })
+
   constructor(root: HTMLElement) {
     super({ tag: 'header', attribute: { className: 'header' }, parent: root })
+    this.navigationButtonGarage.setEventListener('click', ({ target }) => this.changeHashPage(target))
+    this.navigationButtonWinners.setEventListener('click', ({ target }) => this.changeHashPage(target))
+  }
 
-    this.element.innerHTML = '<h1 class = "header__logo">RSS-CSS</h1>'
+  private changeHashPage(target: EventTarget | null): void {
+    if (!(target instanceof HTMLElement) || target.textContent === null) {
+      return
+    }
+
+    window.location.hash = `#${target.textContent.toLowerCase()}`
   }
 }

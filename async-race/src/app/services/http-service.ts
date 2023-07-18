ace5eaec76470023d83e9643818f53ba9f617817
ADyBaH <Brandon4@mail.ru>
2023-07-19 01:00:53 +0300

    feat: add removeCar and addCars methods

--- async-race/src/app/services/http-service.ts
+++ async-race/src/app/services/http-service.ts
@@ -1,11 +1,14 @@
 import { urlServiceString } from '../constants/http-service-constants'
 import type { Car } from '../types/car-type'
 import { emitter } from './event-emitter'
+import { instanceRandomCars } from './random-cars-service'
 
 export class HttpService {
   private serverUrl = urlServiceString
+  private nameCars = instanceRandomCars
   constructor() {
     emitter.subscribe('addCar', this.addCar)
+    this.nameCars.generateRandomCar()
   }
 
   public async getCars(): Promise<Car[]> {
@@ -14,13 +17,25 @@ export class HttpService {
     return getCarsJson
   }
 
-  public async addCar(object: { name: string; color: string }): Promise<void> {
+  public async addCar(object: Omit<Car, 'id'>): Promise<void> {
     await fetch(`${this.serverUrl}/garage`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(object),
+    })
+  }
+
+  public async addCars(): Promise<void> {
+    instanceRandomCars.generateRandomCars().forEach(async (randomCarObject: Omit<Car, 'id'>) => {
+      await this.addCar(randomCarObject)
+    })
+  }
+
+  public async removeCar(id: number): Promise<void> {
+    await fetch(`${this.serverUrl}/garage/${id}`, {
+      method: 'DELETE',
     })
   }
 }

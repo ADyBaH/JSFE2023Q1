dcade17ce4725930629f361abe7cdb01bcfd8109
ADyBaH <Brandon4@mail.ru>
2023-07-18 16:48:50 +0300

    refactor: change BaseComponents to elements

--- async-race/src/app/pages/garage-page/garage.ts
+++ async-race/src/app/pages/garage-page/garage.ts
@@ -1,7 +1,12 @@
+import { Pagination } from 'src/app/components/pagination/pagination'
 import { BaseComponent } from 'src/app/components/base-component'
-import { carSvgString } from 'src/app/constants/car-svg-string'
-import './garage.scss'
+import { Button } from 'src/app/components/button'
+import { httpService } from 'src/app/services/http-service'
+import { emitter } from 'src/app/services/event-emitter'
+import { EmitterEnum } from 'src/app/enum/emitter-enum'
 import { GarageForm } from './components/form/garage-form'
+import { GarageList } from './components/garage-list/garage-list'
+import './garage.scss'
 
 export class Garage extends BaseComponent {
   private inputsContainer = new BaseComponent({
@@ -11,36 +16,9 @@ export class Garage extends BaseComponent {
 
   private createForm = new GarageForm('garage-form', 'Create Car', this.inputsContainer.element)
   private changeForm = new GarageForm('change-form', 'Change Car', this.inputsContainer.element)
-
-  private buttonCreate = new BaseComponent({
-    tag: 'button',
-    attribute: {
-      className: 'button-race button',
-      textContent: 'Race',
-      type: 'button',
-    },
-    parent: this.inputsContainer.element,
-  })
-
-  private buttonReset = new BaseComponent({
-    tag: 'button',
-    attribute: {
-      className: 'button-reset button',
-      textContent: 'Reset',
-      type: 'button',
-    },
-    parent: this.inputsContainer.element,
-  })
-
-  private buttonGenerateCar = new BaseComponent({
-    tag: 'button',
-    attribute: {
-      className: 'button-generate-car button',
-      textContent: 'Generate Car',
-      type: 'button',
-    },
-    parent: this.inputsContainer.element,
-  })
+  private buttonCreate = new Button('button-race button', 'Race', this.inputsContainer.element)
+  private buttonReset = new Button('button-reset button', 'Reset', this.inputsContainer.element)
+  private buttonGenerateCar = new Button('button-generate-car button', 'Generate Car', this.inputsContainer.element)
 
   private logo = new BaseComponent({
     tag: 'h1',
@@ -50,10 +28,10 @@ export class Garage extends BaseComponent {
     },
     parent: this.element,
   })
-  private car = new BaseComponent({
-    attribute: { className: 'container-car', innerHTML: carSvgString },
-    parent: this.element,
-  })
+
+  private garageList = new GarageList(this.element)
+
+  private pagination = new Pagination(this.element)
 
   constructor(parent?: HTMLElement) {
     super({
@@ -62,12 +40,18 @@ export class Garage extends BaseComponent {
       },
       parent,
     })
-    this.createForm.setEventListener('submit', (event) => this.submitCreateFrom(event))
-    this.changeForm.setEventListener('submit', (event) => this.submitCreateFrom(event))
+    this.changeLogo()
+    this.createForm.buttonSubmit.setEventListener('click', this.appendCar)
+    emitter.subscribe(EmitterEnum.updateCars, this.changeLogo)
   }
 
-  private submitCreateFrom(event: Event): void {
-    event.preventDefault()
-    this.car.element.style.fill = this.createForm.color
+  private changeLogo = async (): Promise<void> => {
+    const array = await httpService.getCars()
+    this.logo.innerText = `Garage(${array.length} cars)`
+  }
+
+  private appendCar = (): void => {
+    httpService.addCar({ name: this.createForm.text, color: this.createForm.color })
+    emitter.emit(EmitterEnum.updateCars)
   }
 }

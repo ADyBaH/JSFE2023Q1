c455232669c4e39b40f50ebe4e18767bae9d4ed6
ADyBaH <Brandon4@mail.ru>
2023-07-21 19:55:15 +0300

    feat: add type GarageState

--- async-race/src/app/types/garage-state-type.ts
+++ async-race/src/app/types/garage-state-type.ts
@@ -1,6 +1,9 @@
+import type { GarageListComponent } from '../pages/garage-page/components/garage-list-component/garage-list-component'
+
 export type GarageState = {
   currentPage: number
   maxPage: number
   minPage: number
   changeCar: null | number
+  arrayCars: GarageListComponent[] | null
 }

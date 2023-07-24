2a3a93f07acfd713655a56b9dda0f1b59aa8b076
ADyBaH <Brandon4@mail.ru>
2023-07-24 15:13:51 +0300

    feat: add http methods and response enums

---
+++ async-race/src/app/enum/http-methods-enum.ts
@@ -0,0 +1,6 @@
+export enum HttpMethods {
+  Put = 'PUT',
+  Delete = 'DELETE',
+  Patch = 'PATCH',
+  Post = 'POST',
+}
---
+++ async-race/src/app/enum/response-enum.ts
@@ -0,0 +1,4 @@
+export enum ResponseEnum {
+  Error404 = 404,
+  Error500 = 500,
+}

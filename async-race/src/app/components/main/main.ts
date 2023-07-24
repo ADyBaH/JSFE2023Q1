a2f864850cad84e4382626392e374dc7d252a350
ADyBaH <Brandon4@mail.ru>
2023-07-24 14:19:12 +0300

    feat: add modal for main

--- async-race/src/app/components/main/main.ts
+++ async-race/src/app/components/main/main.ts
@@ -3,23 +3,18 @@ import { Winners } from 'src/app/pages/winners-page/winners-page'
 import { Garage } from 'src/app/pages/garage-page/garage'
 import { Router } from 'src/app/utils/router'
 import { BaseComponent } from '../base-component'
-import { Modal } from './modal/modal'
 import './main.scss'
 
 export class Main extends BaseComponent {
-  private modal: Modal = new Modal(this.element)
-
   private pages = {
     '#garage': new Garage(),
     '#winners': new Winners(),
     'notFound': new NotFound(),
   }
 
-  private router: Router
+  private router = new Router(this.pages, this.element)
 
   constructor(root: HTMLElement) {
     super({ tag: 'main', attribute: { className: 'main' }, parent: root })
-
-    this.router = new Router(this.pages, this.element)
   }
 }

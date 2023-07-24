7841d51475af194d08cd9e0792a6814c2c742b04
ADyBaH <Brandon4@mail.ru>
2023-07-24 16:46:55 +0300

    refactor: add better enums for garage and remuve all magick string and number

--- async-race/src/app/pages/garage-page/garage.ts
+++ async-race/src/app/pages/garage-page/garage.ts
@@ -1,10 +1,13 @@
 import { instanceRandomCars } from 'src/app/services/random-cars-service'
 import { Pagination } from 'src/app/components/pagination/pagination'
 import { BaseComponent } from 'src/app/components/base-component'
+import { ButtonsTextEnum } from 'src/app/enum/buttons-text-enum'
 import { httpService } from 'src/app/services/http-service'
 import { emitter } from 'src/app/services/event-emitter'
 import { EmitterEnum } from 'src/app/enum/emitter-enum'
 import { Button } from 'src/app/components/button'
+import type { StatusCar } from 'src/app/types/status-car-type'
+import { defaultGarageLogo } from './constants/default-garage-logo'
 import { GarageList } from './components/garage-list/garage-list'
 import { GarageForm } from './components/form/garage-form'
 import { garageState } from './garage-state'
@@ -12,27 +15,38 @@ import './garage.scss'
 
 export class Garage extends BaseComponent {
   private garageState = garageState
+
   private inputsContainer = new BaseComponent({
     attribute: { className: 'inputs-container' },
     parent: this.element,
   })
 
   private createForm = new GarageForm('garage-form', 'Create Car', this.inputsContainer.element)
-  private changeForm = new GarageForm('change-form change-form_hide', 'Change Car', this.inputsContainer.element)
-  private buttonRace = new Button('button-race button', 'Race', this.inputsContainer.element)
-  private buttonReset = new Button('button-reset button', 'Reset', this.inputsContainer.element)
-  private buttonGenerateCar = new Button('button-generate-car button', 'Generate Car', this.inputsContainer.element)
+  private changeForm = new GarageForm('change-form', 'Change Car', this.inputsContainer.element)
+
+  private buttonsContainer = new BaseComponent({
+    attribute: { className: 'buttons-container' },
+    parent: this.inputsContainer.element,
+  })
+
+  private buttonRace = new Button('button-race', ButtonsTextEnum.Race, this.buttonsContainer.element)
+  private buttonReset = new Button('button-reset', ButtonsTextEnum.Reset, this.buttonsContainer.element)
+  private buttonGenerateCar = new Button(
+    'button-generate-car',
+    ButtonsTextEnum.GenerateCar,
+    this.buttonsContainer.element,
+  )
 
   private logo = new BaseComponent({
     tag: 'h1',
     attribute: {
       className: 'garage__logo',
-      textContent: 'Garage(0 cars)',
+      textContent: defaultGarageLogo,
     },
     parent: this.element,
   })
 
-  private paginationGarage = new Pagination(this.garageState, this.element)
+  private paginationGarage = new Pagination(this.garageState, this.element, EmitterEnum.ChangeNumberGaragePage)
   private garageList = new GarageList(this.element)
 
   constructor(parent?: HTMLElement) {
@@ -42,18 +56,22 @@ export class Garage extends BaseComponent {
       },
       parent,
     })
+
     this.buttonReset.disableButton()
-    this.createForm.buttonSubmit.setEventListener('click', this.appendCar)
-    this.changeForm.buttonSubmit.setEventListener('click', this.changeCar)
-    this.buttonGenerateCar.setEventListener('click', this.appendCars)
+    this.changeForm.addClass('change-form_hide')
+
     this.buttonRace.setEventListener('click', this.startRace)
     this.buttonReset.setEventListener('click', this.resetRace)
-    emitter.subscribe(EmitterEnum.changeLogo, this.changeLogo)
-    emitter.subscribe(EmitterEnum.changeNumberPage, this.resetRace)
-    emitter.subscribe(EmitterEnum.selectCar, this.selectCar)
-    emitter.subscribe(EmitterEnum.selectCar, this.showChangeForm)
-    emitter.subscribe(EmitterEnum.lockGaragePaginationButtons, this.paginationGarage.disableAllPaginationButtons)
-    emitter.subscribe(EmitterEnum.unlockGaragePaginationButtons, this.paginationGarage.checkButtons)
+    this.buttonGenerateCar.setEventListener('click', this.appendCars)
+    this.createForm.buttonSubmit.setEventListener('click', this.appendCar)
+    this.changeForm.buttonSubmit.setEventListener('click', this.changeCar)
+
+    emitter.subscribe(EmitterEnum.ChangeLogo, this.changeLogo)
+    emitter.subscribe(EmitterEnum.SelectCar, this.selectCar)
+    emitter.subscribe(EmitterEnum.SelectCar, this.showChangeForm)
+    emitter.subscribe(EmitterEnum.ChangeNumberGaragePage, this.resetRace)
+    emitter.subscribe(EmitterEnum.UnlockGaragePaginationButtons, this.paginationGarage.checkButtons)
+    emitter.subscribe(EmitterEnum.LockGaragePaginationButtons, this.paginationGarage.disableAllPaginationButtons)
   }
 
   private startRace = (): void => {
@@ -76,16 +94,17 @@ export class Garage extends BaseComponent {
   private appendCar = async (): Promise<void> => {
     const nameCar = this.createForm.text || instanceRandomCars.generateRandomCar().name
     await httpService.addCar({ name: nameCar, color: this.createForm.color })
-    emitter.emit(EmitterEnum.updateCars)
+    emitter.emit(EmitterEnum.UpdateCars)
   }
 
   private appendCars = async (): Promise<void> => {
     await httpService.addCars()
-    emitter.emit(EmitterEnum.updateCars)
   }
 
-  private selectCar = (numberCar: number): void => {
-    this.garageState.changeCar = numberCar
+  private selectCar = (car: StatusCar): void => {
+    this.garageState.changeCar = car
+    this.changeForm.text = this.garageState.changeCar.carName.innerText
+    this.changeForm.color = this.garageState.changeCar.carColor
   }
 
   private hideChangeForm = (): void => {
@@ -101,17 +120,17 @@ export class Garage extends BaseComponent {
       return
     }
 
-    const getCarFromServer = await httpService.getCar(this.garageState.changeCar)
+    const getCarFromServer = await httpService.getCar(this.garageState.changeCar.id)
     if (!getCarFromServer.name) {
       this.hideChangeForm()
       this.garageState.changeCar = null
       return
     }
 
-    const nameCar = this.changeForm.text || instanceRandomCars.generateRandomCar().name
-    await httpService.changeCar({ name: nameCar, color: this.changeForm.color }, this.garageState.changeCar)
+    const nameCar = this.changeForm.text || getCarFromServer.name
+    await httpService.changeCar({ name: nameCar, color: this.changeForm.color }, this.garageState.changeCar.id)
     this.garageState.changeCar = null
     this.hideChangeForm()
-    emitter.emit(EmitterEnum.updateCars)
+    emitter.emit(EmitterEnum.UpdateCars)
   }
 }

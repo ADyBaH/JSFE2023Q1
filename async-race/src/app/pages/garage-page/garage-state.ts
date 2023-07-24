5e4f0e3f51e999fc06a02b1ead49699e281d4499
ADyBaH <Brandon4@mail.ru>
2023-07-24 16:30:08 +0300

    refactor: remove magick number and string for state

--- async-race/src/app/pages/garage-page/garage-state.ts
+++ async-race/src/app/pages/garage-page/garage-state.ts
@@ -1,9 +1,10 @@
+import { defaultValuesState } from 'src/app/constants/state-constants'
 import type { GarageState } from 'src/app/types/garage-state-type'
 
 export const garageState: GarageState = {
-  currentPage: 1,
-  maxPage: 1,
-  minPage: 1,
-  changeCar: null,
-  arrayCars: null,
+  currentPage: defaultValuesState.currentPage,
+  maxPage: defaultValuesState.maxPage,
+  minPage: defaultValuesState.minPage,
+  changeCar: defaultValuesState.changeCar,
+  arrayCars: defaultValuesState.arrayCars,
 }
